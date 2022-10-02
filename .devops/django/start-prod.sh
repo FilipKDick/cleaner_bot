6026058f671f8e86b1d51a82e633bf12894e55f9
Kasia <katarzyna.jagiela@tylko.com>
2022-10-02 17:01:02 +0200

    Create superuser

--- .devops/django/start-prod.sh
+++ .devops/django/start-prod.sh
@@ -6,5 +6,6 @@ set -o nounset
 
 
 python /app/manage.py collectstatic --noinput
+python /app/manage.py createsuperuser --noinput
 
 /usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:8000 --chdir=/app
