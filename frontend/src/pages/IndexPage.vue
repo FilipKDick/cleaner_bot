7034f022ef05f640b989b93e4d8ffd389f6df508
Kasia <katarzyna.jagiela@tylko.com>
2022-10-02 20:32:32 +0200

    Frontend scaffold

---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Cleaner bot logo"
+      src="~assets/logo.jpg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
