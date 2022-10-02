179632a9ebebf15acaea3efb31cd5140c18971f3
Kasia <katarzyna.jagiela@tylko.com>
2022-10-02 12:06:20 +0200

    Comment out anymail for now

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -85,16 +85,16 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 # Anymail
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-INSTALLED_APPS += ['anymail']
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-# https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
-# https://anymail.readthedocs.io/en/stable/esps/mailgun/
-EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
-ANYMAIL = {
-    'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN'),
-    'MAILGUN_API_URL': env('MAILGUN_API_URL', default='https://api.mailgun.net/v3'),
-}
+# INSTALLED_APPS += ['anymail']
+# # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+# # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
+# # https://anymail.readthedocs.io/en/stable/esps/mailgun/
+# EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+# ANYMAIL = {
+#     'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
+#     'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN'),
+#     'MAILGUN_API_URL': env('MAILGUN_API_URL', default='https://api.mailgun.net/v3'),
+# }
 
 
 # LOGGING
