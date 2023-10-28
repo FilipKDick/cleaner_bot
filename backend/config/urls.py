bfe210215f9cebbdacb12503f3ed50b824515c90
Kasia <katarzyna.jagiela@tylko.com>
2023-10-28 19:32:38 +0200

    Update

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,10 +22,10 @@ urlpatterns = [
 ] + static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT,
-)  # type: ignore
+)
 
 if settings.DEBUG:
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
